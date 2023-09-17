3c158b7d766d0db604b12f10ad7cbb59e7a3e61e
Example <example@example.org>
2023-09-17 15:46:26 +0200

    Added a readFile function

--- include/main.hpp
+++ include/main.hpp
@@ -1,2 +1,5 @@
 std::string sourceFilePath;
 long sourceFileSize;
+std::string destFilePath;
+std::string line;
+std::string sourceContent;
--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,25 @@ void askForAVariable(const std::string sentence, std::string* path) {
     std::cin >> *path;
 }
 
+std::string readFile(std::string path) {
+    std::string string;
+    std::ifstream file(path, std::ios::in | std::ios::binary);
+    while(getline(file, line)) {
+        string += line + "\n";
+    }
+    file.close();
+    return string;
+}
+
 int main() {
-    std::string ask = "Please enter the path of the file you want to copy: ";
-    askForAVariable(ask, &sourceFilePath);
+    askForAVariable("Please enter the source path: ", &sourceFilePath);
     sourceFileSize = getFileSize(sourceFilePath);
     if (sourceFileSize == false) {
         std::cout << "Could not read source file" << std::endl;
         return 1;
     }
+    askForAVariable("Please enter the destination path: ", &destFilePath);
+    std::string sourceContent = readFile(sourceFilePath);
+
     return 0;
 }
