0a485c7619dac5399d52e356e1c26abcb6a18a9a
Example <example@example.org>
2023-09-17 14:58:26 +0200

    Added a getFileSize function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "main.hpp"
+#include <fstream>
+
+std::ifstream::pos_type getFileSize(const std::string filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    if(in.is_open()) {
+        return in.tellg(); 
+    } else {
+        return false;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Please enter the path of the file you want to copy: ";
+    std::cin >> sourceFilePath;
+    sourceFileSize = getFileSize();
+    if (sourceFileSize == false) {
+        std::cout << "Could not read source file" << std::endl;
+        return 1;
+    }
+    return 0;
 }
